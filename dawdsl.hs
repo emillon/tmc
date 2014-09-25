c0d09205b4bb2247e533c4161067fb354b93b5cb
Etienne Millon <me@emillon.org>
2014-09-25 17:23:49 +0200

    free monad rewrite

--- dawdsl.hs
+++ dawdsl.hs
@@ -1,6 +1,6 @@
 import Control.Monad
+import Control.Monad.Free
 import System.Cmd
-import System.Directory
 
 decodeMp3 :: FilePath -> FilePath -> IO ()
 decodeMp3 input output =
@@ -26,20 +26,27 @@ gainFile :: FilePath -> Double -> FilePath -> IO ()
 gainFile input amount output =
     void $ system $ "sox " ++ input ++ " " ++ output ++ " gain " ++ show amount
 
-main :: IO ()
-main = do
-    let acapFile = "katy.wav"
-        acapBPM = 125
-        instruFile = "walkonby.wav"
-        instruBPM = 133
-        warpedAcapFile = "katyW.wav"
+warpRatio :: Double
+warpRatio = newBPM / oldBPM
+    where
         newBPM = instruBPM
         oldBPM = acapBPM
-        warpRatio = newBPM / oldBPM
+        acapBPM = 125
+        instruBPM = 133
+
+shiftAmount :: Double
+shiftAmount = 5.899
+
+gainAmount :: Double
+gainAmount = -3
+
+main0 :: IO ()
+main0 = do
+    let acapFile = "katy.wav"
+        instruFile = "walkonby.wav"
+        warpedAcapFile = "katyW.wav"
         shiftedAcapFile = "katyWS.wav"
-        shiftAmount = 5.899
         gainAcapFile = "katyWSG.wav"
-        gainAmount = -3
         resultFile = "res.wav"
     decodeMp3 "katy.mp3" acapFile
     decodeFlac "walkonby.flac" instruFile
@@ -47,3 +54,93 @@ main = do
     shiftFile warpedAcapFile shiftAmount shiftedAcapFile
     gainFile shiftedAcapFile gainAmount gainAcapFile
     mergeFiles gainAcapFile instruFile resultFile
+
+data ProgF a = MP3File FilePath (Audio -> a)
+             | FlacFile FilePath (Audio -> a)
+             | Warp Double a
+             | Shift Double a
+             | Gain Double a
+             | Merge a a
+
+instance Functor ProgF where
+    fmap f (MP3File path k) = MP3File path (f . k)
+    fmap f (FlacFile path k) = FlacFile path (f . k)
+    fmap f (Warp ratio k) = Warp ratio (f k)
+    fmap f (Shift a k) = Shift a (f k)
+    fmap f (Gain a k) = Gain a (f k)
+    fmap f (Merge a b) = Merge (f a) (f b)
+
+type Prog a = Free ProgF a
+
+data Audio = Audio FilePath
+
+flacFile :: FilePath -> Prog Audio
+flacFile fp = liftF $ FlacFile fp id
+
+mp3File :: FilePath -> Prog Audio
+mp3File fp = liftF $ MP3File fp id
+
+warpAudio :: Double -> Audio -> Prog Audio
+warpAudio ratio = liftF . Warp ratio
+
+shiftAudio :: Double -> Audio -> Prog Audio
+shiftAudio amount = liftF . Shift amount
+
+gainAudio :: Double -> Audio -> Prog Audio
+gainAudio amount = liftF . Gain amount
+
+mergeAudio :: Audio -> Audio -> Prog Audio
+mergeAudio a b = liftF $ Merge a b
+
+p :: Prog Audio
+p = do
+    acap <- mp3File "katy.mp3"
+    instr <- flacFile "walkonby.flac"
+    warpedAcap <- warpAudio warpRatio acap
+    shiftedAcap <- shiftAudio shiftAmount warpedAcap
+    gainAcap <- gainAudio gainAmount shiftedAcap
+    mergeAudio instr gainAcap
+
+run :: Prog Audio -> IO Audio
+run (Pure x) = return x
+run (Free (MP3File path k)) = do
+    let temp = "decodemp3." ++ path ++ ".wav"
+    decodeMp3 path temp
+    run $ k $ Audio temp
+run (Free (FlacFile path k)) = do
+    let temp = "decodeflac." ++ path ++ ".wav"
+    decodeFlac path temp
+    run $ k $ Audio temp
+run (Free (Warp amount k)) = do
+    Audio input <- run k
+    let temp = "warp" ++ show amount ++ "." ++ input
+    warpFile input amount temp
+    return $ Audio temp
+run (Free (Shift amount k)) = do
+    Audio input <- run k
+    let temp = "shift" ++ show amount ++ "." ++ input
+    shiftFile input amount temp
+    return $ Audio temp
+run (Free (Gain amount k)) = do
+    Audio input <- run k
+    let temp = "gain" ++ show amount ++ "." ++ input
+    gainFile input amount temp
+    return $ Audio temp
+run (Free (Merge ka kb)) = do
+    Audio a <- run ka
+    Audio b <- run kb
+    let temp = a ++ "merged" ++ b
+    mergeFiles a b temp
+    return $ Audio temp
+
+steps :: Prog Audio -> [String]
+steps (Pure _) = undefined
+steps (Free (MP3File _ k)) = "decode mp3" : steps (k (Audio undefined))
+steps (Free (FlacFile _ k)) = "decode flac" : steps (k (Audio undefined))
+steps (Free (Warp amount r)) = ("warp " ++ show amount) : steps r
+steps (Free (Shift amount r)) = ("shift " ++ show amount) : steps r
+steps (Free (Gain amount r)) = ("gain " ++ show amount) : steps r
+steps (Free (Merge _ _)) = ["merge"]
+
+main :: IO ()
+main = void $ run p
