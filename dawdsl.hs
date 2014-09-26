c6f81f18107eeeed54f38c8c10f90591d3118e64
Etienne Millon <me@emillon.org>
2014-09-26 15:26:38 +0200

    merge into a Bind

--- dawdsl.hs
+++ dawdsl.hs
@@ -23,29 +23,28 @@ mergeFiles :: FilePath -> FilePath -> FilePath -> IO ()
 mergeFiles a b output =
     execCommand "sox" ["-m", a, b, output]
 
-data ProgF a = MP3File FilePath (Audio -> a)
-             | FlacFile FilePath (Audio -> a)
-             | SoxFX String Audio (Audio -> a)
-             | Merge Audio Audio (Audio -> a)
+data ProgF a = Bind Op (Audio -> a)
+
+data Op = MP3File FilePath
+        | FlacFile FilePath
+        | SoxFX String Audio
+        | Merge Audio Audio
 
 instance Functor ProgF where
-    fmap f (MP3File path k) = MP3File path (f . k)
-    fmap f (FlacFile path k) = FlacFile path (f . k)
-    fmap f (SoxFX fx a k) = SoxFX fx a (f . k)
-    fmap f (Merge a b k) = Merge a b (f . k)
+    fmap f (Bind op k) = Bind op (f . k)
 
 type Prog a = Free ProgF a
 
 data Audio = Audio FilePath
 
 flacFile :: FilePath -> Prog Audio
-flacFile fp = liftF $ FlacFile fp id
+flacFile fp = liftF $ Bind (FlacFile fp) id
 
 mp3File :: FilePath -> Prog Audio
-mp3File fp = liftF $ MP3File fp id
+mp3File fp = liftF $ Bind (MP3File fp) id
 
 soxFX :: String -> Audio -> Prog Audio
-soxFX fx a = liftF $ SoxFX fx a id
+soxFX fx a = liftF $ Bind (SoxFX fx a) id
 
 warpAudio :: Double -> Audio -> Prog Audio
 warpAudio ratio = soxFX $ "tempo " ++ show ratio
@@ -57,7 +56,7 @@ gainAudio :: Double -> Audio -> Prog Audio
 gainAudio amount = soxFX $ "gain " ++ show amount
 
 mergeAudio :: Audio -> Audio -> Prog Audio
-mergeAudio a b = liftF $ Merge a b id
+mergeAudio a b = liftF $ Bind (Merge a b) id
 
 nextTemp :: IO FilePath
 nextTemp = go 0
@@ -79,31 +78,25 @@ p = do
     gainAcap <- gainAudio (-3) shiftedAcap
     mergeAudio instr gainAcap
 
+interpretOp :: Op -> FilePath -> IO ()
+interpretOp (MP3File path) temp = decodeMp3 path temp
+interpretOp (FlacFile path) temp = decodeFlac path temp
+interpretOp (SoxFX fx (Audio input)) temp = applySoxFx input temp fx
+interpretOp (Merge (Audio a) (Audio b)) temp = mergeFiles a b temp
+
 run :: Prog Audio -> IO Audio
 run (Pure x) = return x
-run (Free (MP3File path k)) = do
+run (Free (Bind op k)) = do
     temp <- nextTemp
-    decodeMp3 path temp
-    run $ k $ Audio temp
-run (Free (FlacFile path k)) = do
-    temp <- nextTemp
-    decodeFlac path temp
-    run $ k $ Audio temp
-run (Free (SoxFX fx (Audio input) k)) = do
-    temp <- nextTemp
-    applySoxFx input temp fx
-    run $ k $ Audio temp
-run (Free (Merge (Audio a) (Audio b) k)) = do
-    temp <- nextTemp
-    mergeFiles a b temp
+    interpretOp op temp
     run $ k $ Audio temp
 
 steps :: Prog Audio -> [String]
 steps (Pure _) = []
-steps (Free (MP3File _ k)) = "decode mp3" : steps (k (Audio undefined))
-steps (Free (FlacFile _ k)) = "decode flac" : steps (k (Audio undefined))
-steps (Free (SoxFX fx _ k)) = ("soxfx " ++ fx) : steps (k (Audio undefined))
-steps (Free (Merge _ _ k)) = "merge" : steps (k (Audio undefined))
+steps (Free (Bind (MP3File _) k)) = "decode mp3" : steps (k (Audio undefined))
+steps (Free (Bind (FlacFile _) k)) = "decode flac" : steps (k (Audio undefined))
+steps (Free (Bind (SoxFX fx _) k)) = ("soxfx " ++ fx) : steps (k (Audio undefined))
+steps (Free (Bind (Merge _ _) k)) = "merge" : steps (k (Audio undefined))
 
 main :: IO ()
 main = do
