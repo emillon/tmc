0b98843541fb66133b497e12a72fb88568750878
Etienne Millon <me@emillon.org>
2014-09-26 14:12:16 +0200

    free monads man

--- dawdsl.hs
+++ dawdsl.hs
@@ -1,6 +1,7 @@
 import Control.Monad
 import Control.Monad.Free
 import System.Cmd
+import System.Directory
 
 decodeMp3 :: FilePath -> FilePath -> IO ()
 decodeMp3 input output =
@@ -11,7 +12,7 @@ decodeFlac input output =
     void $ system $ "flac -f --decode " ++ input ++ " -o " ++ output
 
 applySoxFx :: FilePath -> FilePath -> String -> IO ()
-applySoxFx input output fx = do
+applySoxFx input output fx =
     void $ system $ "sox " ++ input ++ " " ++ output ++ " " ++ fx
 
 warpFile :: FilePath -> Double -> FilePath -> IO ()
@@ -19,7 +20,7 @@ warpFile input ratio output = do
     applySoxFx input output $ "speed " ++ show ratio
 
 shiftFile :: FilePath -> Double -> FilePath -> IO ()
-shiftFile input amount output = do
+shiftFile input amount output =
     applySoxFx input output $ "pad " ++ show amount
 
 mergeFiles :: FilePath -> FilePath -> FilePath -> IO ()
@@ -61,14 +62,14 @@ main0 = do
 
 data ProgF a = MP3File FilePath (Audio -> a)
              | FlacFile FilePath (Audio -> a)
-             | SoxFX String a
-             | Merge a a
+             | SoxFX String Audio (Audio -> a)
+             | Merge Audio Audio (Audio -> a)
 
 instance Functor ProgF where
     fmap f (MP3File path k) = MP3File path (f . k)
     fmap f (FlacFile path k) = FlacFile path (f . k)
-    fmap f (SoxFX fx k) = SoxFX fx (f k)
-    fmap f (Merge a b) = Merge (f a) (f b)
+    fmap f (SoxFX fx a k) = SoxFX fx a (f . k)
+    fmap f (Merge a b k) = Merge a b (f . k)
 
 type Prog a = Free ProgF a
 
@@ -80,17 +81,31 @@ flacFile fp = liftF $ FlacFile fp id
 mp3File :: FilePath -> Prog Audio
 mp3File fp = liftF $ MP3File fp id
 
+soxFX :: String -> Audio -> Prog Audio
+soxFX fx a = liftF $ SoxFX fx a id
+
 warpAudio :: Double -> Audio -> Prog Audio
-warpAudio ratio = liftF . SoxFX ("speed " ++ show ratio)
+warpAudio ratio = soxFX $ "speed " ++ show ratio
 
 shiftAudio :: Double -> Audio -> Prog Audio
-shiftAudio amount = liftF . SoxFX ("pad " ++ show amount)
+shiftAudio amount = soxFX $ "pad " ++ show amount
 
 gainAudio :: Double -> Audio -> Prog Audio
-gainAudio amount = liftF . SoxFX ("gain " ++ show amount)
+gainAudio amount = soxFX $ "gain " ++ show amount
 
 mergeAudio :: Audio -> Audio -> Prog Audio
-mergeAudio a b = liftF $ Merge a b
+mergeAudio a b = liftF $ Merge a b id
+
+nextTemp :: IO FilePath
+nextTemp = go 0
+    where
+        go :: Integer -> IO FilePath
+        go n = do
+            let path = "temp" ++ show n ++ ".wav"
+            ex <- doesFileExist path
+            if ex
+                then go (n+1)
+                else return path
 
 p :: Prog Audio
 p = do
@@ -104,31 +119,33 @@ p = do
 run :: Prog Audio -> IO Audio
 run (Pure x) = return x
 run (Free (MP3File path k)) = do
-    let temp = "decodemp3." ++ path ++ ".wav"
+    temp <- nextTemp
     decodeMp3 path temp
     run $ k $ Audio temp
 run (Free (FlacFile path k)) = do
-    let temp = "decodeflac." ++ path ++ ".wav"
+    temp <- nextTemp
     decodeFlac path temp
     run $ k $ Audio temp
-run (Free (SoxFX fx k)) = do
-    Audio input <- run k
-    let temp = "fx" ++ fx ++ "." ++ input
+run (Free (SoxFX fx (Audio input) k)) = do
+    temp <- nextTemp
     applySoxFx input temp fx
+    run $ k $ Audio temp
     return $ Audio temp
-run (Free (Merge ka kb)) = do
-    Audio a <- run ka
-    Audio b <- run kb
-    let temp = a ++ "merged" ++ b
+        where
+            replaceSpc ' ' = '_'
+            replaceSpc c = c
+run (Free (Merge (Audio a) (Audio b) k)) = do
+    temp <- nextTemp
     mergeFiles a b temp
+    run $ k $ Audio temp
     return $ Audio temp
 
 steps :: Prog Audio -> [String]
-steps (Pure _) = undefined
+steps (Pure _) = []
 steps (Free (MP3File _ k)) = "decode mp3" : steps (k (Audio undefined))
 steps (Free (FlacFile _ k)) = "decode flac" : steps (k (Audio undefined))
-steps (Free (SoxFX fx r)) = ("sox fx " ++ fx) : steps r
-steps (Free (Merge _ _)) = ["merge"]
+steps (Free (SoxFX fx _ k)) = ("soxfx " ++ fx) : steps (k (Audio undefined))
+steps (Free (Merge _ _ k)) = "merge" : steps (k (Audio undefined))
 
 main :: IO ()
 main = void $ run p
