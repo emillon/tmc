3f86552aa38c9f5bb91586fb68153319c68e827f
Etienne Millon <me@emillon.org>
2014-09-29 14:53:30 +0200

    add Track

--- Example.hs
+++ Example.hs
@@ -2,11 +2,34 @@ module Example (exampleBootleg) where
 
 import Prog
 
+data Track = Track { trackFormat :: AudioType
+                   , trackPath :: FilePath
+                   , trackBPM :: Double
+                   }
+
+track :: Track -> Prog Audio
+track t =
+    sourceFile (trackFormat t) (trackPath t)
+
+katy :: Track
+katy =
+    Track { trackFormat = Mp3
+          , trackPath = "katy.mp3"
+          , trackBPM = 125
+          }
+
+walkOnBy :: Track
+walkOnBy =
+    Track { trackFormat = Flac
+          , trackPath = "walkonby.flac"
+          , trackBPM = 133
+          }
+
 exampleBootleg :: Prog Audio
 exampleBootleg = do
-    acap <- sourceFile Mp3 "katy.mp3"
-    instr <- sourceFile Flac "walkonby.flac"
-    warpedAcap <- warpAudio (133 / 125) acap
+    acap <- track katy
+    instr <- track walkOnBy
+    warpedAcap <- warpAudio ((trackBPM walkOnBy) / (trackBPM katy)) acap
     shiftedAcap <- shiftAudio 5.899 warpedAcap
     gainAcap <- gainAudio (-3) shiftedAcap
     mergeAudio instr gainAcap
