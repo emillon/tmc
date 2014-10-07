514a2c236bd620748a42f7f7e4014cdc24edc3f1
Etienne Millon <me@emillon.org>
2014-10-07 09:04:53 +0200

    Add a Music.TMC reexport
    Closes #3

---
+++ Music/TMC.hs
@@ -0,0 +1,17 @@
+-- | A module that reexports everything useful.
+
+module Music.TMC
+    ( module Music.TMC.Cache
+    , module Music.TMC.Optimize
+    , module Music.TMC.Prog
+    , module Music.TMC.Run
+    , module Music.TMC.Tools
+    , module Music.TMC.Types
+    ) where
+
+import Music.TMC.Cache
+import Music.TMC.Optimize
+import Music.TMC.Prog
+import Music.TMC.Run
+import Music.TMC.Tools
+import Music.TMC.Types
